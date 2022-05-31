fec6f9036a87b7569b17e7f0dc1c2c40d97827ee
cante-maxence <maxence.cante@edu.univ-fcomte.fr>
2022-05-31 21:25:27 +0200

    rajout du CDN Fire base

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCrfyEmfWnzEEnQQZuRe5ZS9sFDHVYxoKo",
+    authDomain: "sae-203-74962.firebaseapp.com",
+    projectId: "sae-203-74962",
+    storageBucket: "sae-203-74962.appspot.com",
+    messagingSenderId: "654295469862",
+    appId: "1:654295469862:web:0a16fc321f1e8fa0003be7"
+};
+
+const appFirebase = initializeApp(firebaseConfig);
+
 const app = createApp(App)
 
 app.use(router)
